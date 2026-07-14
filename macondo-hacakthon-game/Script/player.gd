a909f47a0b837b74835d36ef9745d057420565e3
Nicolas Rozo <nicolasrc14@outlook.com>
2026-07-14 13:17:04 -0400

    Most Stuff
    Player movement, coin collection, tile map art, level scenes

--- macondo-hacakthon-game/Script/player.gd
+++ macondo-hacakthon-game/Script/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 300
 
-var last_direction: Vector2 = Vector2.RIGHT
+var last_direction: Vector2 = Vector2.DOWN
+
+var coin_counter = 0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -12,27 +14,30 @@ func _physics_process(_delta: float) -> void:
 	process_movement()
 	process_animation()
 	move_and_slide()
-
-
+	
+	
 func process_movement() -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector("Left", "Right", "Up", "Down")
+	
+	var direction := Input.get_vector("Left", "Right","Up","Down")
+	velocity = direction * SPEED
 	
 	if direction != Vector2.ZERO:
-		velocity = direction * SPEED
+		velocity = direction *SPEED
 		last_direction = direction
 	else:
 		velocity = Vector2.ZERO
 	
+
+
 
 func process_animation() -> void:
 	if velocity != Vector2.ZERO:
 		play_animation("running", last_direction)
 	else:
 		play_animation("idle", last_direction)
-	
-func play_animation(prefix: String, dir: Vector2) -> void:
+
+
+func play_animation(prefix: String, dir: Vector2)  -> void:
 	if dir.x != 0:
 		animated_sprite_2d.flip_h = dir.x < 0
 		animated_sprite_2d.play(prefix + "_right")
@@ -40,3 +45,16 @@ func play_animation(prefix: String, dir: Vector2) -> void:
 		animated_sprite_2d.play(prefix + "_up")
 	elif dir.y > 0:
 		animated_sprite_2d.play(prefix + "_down")
+		
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("coin"):
+		set_coin(coin_counter + 1)
+		print (coin_counter)
+			
+		
+
+func set_coin(new_coin_count: int) -> void: 
+	coin_counter = new_coin_count
+	
